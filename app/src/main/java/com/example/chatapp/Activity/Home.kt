31aa4b4f831cf09108d2ad03bc07d1bf9f84253a
ishant <ishantkuwt@gmail.com>
2023-12-11 12:00:34 +0530

    commit 3 adapter

--- app/src/main/java/com/example/chatapp/Activity/Home.kt
+++ app/src/main/java/com/example/chatapp/Activity/Home.kt
@@ -3,10 +3,14 @@ package com.example.chatapp.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
